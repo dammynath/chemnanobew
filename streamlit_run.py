76107d806f5e04f8ff4b770d5f9c30702f75a61c
Nathanael Damilare Ojo <132015195+dammynath@users.noreply.github.com>
2026-02-19 09:56:47 +0200

    streamlit_run.py

--- streamlit_run.py
+++ streamlit_run.py
@@ -8,14 +8,9 @@ Created on Thu Feb 19 08:55:36 2026
 import subprocess
 
 file = "app.py"
-#file = "app_plots.py"
-#file = "app_profiler.py"
-#file = "app_profiler_menus.py"
-#file = "Streamlit_day3.py"
-#file = "app.py"
-
 
 subprocess.Popen(
     ["streamlit", "run", file], shell=True
 )
+
 
